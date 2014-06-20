6bd62957393243ad93c6a960739a2683499ea5b1
Captain Murphy <captain@teamduck.com>
2014-06-20 09:15:12 -0500

    Initial Check in

---
+++ Form1.Designer.cs
@@ -0,0 +1,352 @@
+﻿namespace Hearts_of_Oak_Packager
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.fbdCERoot = new System.Windows.Forms.FolderBrowserDialog();
+            this.txbCERoot = new System.Windows.Forms.TextBox();
+            this.btnCERootBrowse = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.tsbSettings = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.loadToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.btnGamePath = new System.Windows.Forms.Button();
+            this.txbGamePath = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.cbxCompressGame = new System.Windows.Forms.CheckBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnOutput = new System.Windows.Forms.Button();
+            this.txbOutput = new System.Windows.Forms.TextBox();
+            this.bwpMove = new System.ComponentModel.BackgroundWorker();
+            this.btnBuild = new System.Windows.Forms.Button();
+            this.lblFileStatus = new System.Windows.Forms.Label();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.tmrMtc = new System.Windows.Forms.Timer(this.components);
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.cbxCompressionLevel = new System.Windows.Forms.ComboBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // fbdCERoot
+            // 
+            this.fbdCERoot.ShowNewFolderButton = false;
+            // 
+            // txbCERoot
+            // 
+            this.txbCERoot.Location = new System.Drawing.Point(15, 66);
+            this.txbCERoot.Name = "txbCERoot";
+            this.txbCERoot.Size = new System.Drawing.Size(338, 20);
+            this.txbCERoot.TabIndex = 0;
+            this.txbCERoot.Text = "F:\\Projects\\Steam\\SteamApps\\common\\CRYENGINE";
+            // 
+            // btnCERootBrowse
+            // 
+            this.btnCERootBrowse.Location = new System.Drawing.Point(359, 64);
+            this.btnCERootBrowse.Name = "btnCERootBrowse";
+            this.btnCERootBrowse.Size = new System.Drawing.Size(75, 23);
+            this.btnCERootBrowse.TabIndex = 1;
+            this.btnCERootBrowse.Text = "Browse...";
+            this.btnCERootBrowse.UseVisualStyleBackColor = true;
+            this.btnCERootBrowse.Click += new System.EventHandler(this.btnCERootBrowse_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 50);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(152, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Hearts Of Oak CryEngine Root";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tsbSettings});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(456, 24);
+            this.menuStrip1.TabIndex = 3;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // tsbSettings
+            // 
+            this.tsbSettings.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.saveToolStripMenuItem,
+            this.loadToolStripMenuItem,
+            this.toolStripSeparator1,
+            this.exitToolStripMenuItem});
+            this.tsbSettings.Name = "tsbSettings";
+            this.tsbSettings.Size = new System.Drawing.Size(61, 20);
+            this.tsbSettings.Text = "Settings";
+            // 
+            // saveToolStripMenuItem
+            // 
+            this.saveToolStripMenuItem.Name = "saveToolStripMenuItem";
+            this.saveToolStripMenuItem.Size = new System.Drawing.Size(100, 22);
+            this.saveToolStripMenuItem.Text = "Save";
+            // 
+            // loadToolStripMenuItem
+            // 
+            this.loadToolStripMenuItem.Name = "loadToolStripMenuItem";
+            this.loadToolStripMenuItem.Size = new System.Drawing.Size(100, 22);
+            this.loadToolStripMenuItem.Text = "Load";
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(97, 6);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(100, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // btnGamePath
+            // 
+            this.btnGamePath.Location = new System.Drawing.Point(359, 116);
+            this.btnGamePath.Name = "btnGamePath";
+            this.btnGamePath.Size = new System.Drawing.Size(75, 23);
+            this.btnGamePath.TabIndex = 5;
+            this.btnGamePath.Text = "Browse...";
+            this.btnGamePath.UseVisualStyleBackColor = true;
+            this.btnGamePath.Click += new System.EventHandler(this.btnGamePath_Click);
+            // 
+            // txbGamePath
+            // 
+            this.txbGamePath.Location = new System.Drawing.Point(15, 118);
+            this.txbGamePath.Name = "txbGamePath";
+            this.txbGamePath.Size = new System.Drawing.Size(338, 20);
+            this.txbGamePath.TabIndex = 4;
+            this.txbGamePath.Text = "F:\\Projects\\Steam\\SteamApps\\common\\CRYENGINE\\Game";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 102);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(131, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Hearts Of Oak Game Path";
+            // 
+            // cbxCompressGame
+            // 
+            this.cbxCompressGame.AutoSize = true;
+            this.cbxCompressGame.Checked = true;
+            this.cbxCompressGame.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.cbxCompressGame.Location = new System.Drawing.Point(36, 144);
+            this.cbxCompressGame.Name = "cbxCompressGame";
+            this.cbxCompressGame.Size = new System.Drawing.Size(128, 17);
+            this.cbxCompressGame.TabIndex = 7;
+            this.cbxCompressGame.Text = "Compress Game Path";
+            this.cbxCompressGame.UseVisualStyleBackColor = true;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 246);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(83, 13);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Output Location";
+            // 
+            // btnOutput
+            // 
+            this.btnOutput.Location = new System.Drawing.Point(359, 260);
+            this.btnOutput.Name = "btnOutput";
+            this.btnOutput.Size = new System.Drawing.Size(75, 23);
+            this.btnOutput.TabIndex = 9;
+            this.btnOutput.Text = "Browse...";
+            this.btnOutput.UseVisualStyleBackColor = true;
+            this.btnOutput.Click += new System.EventHandler(this.btnOutput_Click);
+            // 
+            // txbOutput
+            // 
+            this.txbOutput.Location = new System.Drawing.Point(15, 262);
+            this.txbOutput.Name = "txbOutput";
+            this.txbOutput.Size = new System.Drawing.Size(338, 20);
+            this.txbOutput.TabIndex = 8;
+            this.txbOutput.Text = "F:\\Projects\\CRYENGINE\\OutputTest";
+            // 
+            // bwpMove
+            // 
+            this.bwpMove.WorkerReportsProgress = true;
+            this.bwpMove.WorkerSupportsCancellation = true;
+            this.bwpMove.DoWork += new System.ComponentModel.DoWorkEventHandler(this.bwpMove_DoWork);
+            this.bwpMove.ProgressChanged += new System.ComponentModel.ProgressChangedEventHandler(this.bwpMove_ProgressChanged);
+            // 
+            // btnBuild
+            // 
+            this.btnBuild.Location = new System.Drawing.Point(15, 336);
+            this.btnBuild.Name = "btnBuild";
+            this.btnBuild.Size = new System.Drawing.Size(75, 23);
+            this.btnBuild.TabIndex = 11;
+            this.btnBuild.Text = "Build";
+            this.btnBuild.UseVisualStyleBackColor = true;
+            this.btnBuild.Click += new System.EventHandler(this.btnBuild_Click);
+            // 
+            // lblFileStatus
+            // 
+            this.lblFileStatus.AutoSize = true;
+            this.lblFileStatus.Location = new System.Drawing.Point(12, 394);
+            this.lblFileStatus.Name = "lblFileStatus";
+            this.lblFileStatus.Size = new System.Drawing.Size(60, 13);
+            this.lblFileStatus.TabIndex = 12;
+            this.lblFileStatus.Text = "Current File";
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(12, 410);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(432, 23);
+            this.progressBar1.Style = System.Windows.Forms.ProgressBarStyle.Marquee;
+            this.progressBar1.TabIndex = 13;
+            // 
+            // tmrMtc
+            // 
+            this.tmrMtc.Enabled = true;
+            this.tmrMtc.Tick += new System.EventHandler(this.tmrMtc_Tick);
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Enabled = false;
+            this.btnCancel.Location = new System.Drawing.Point(96, 336);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(75, 23);
+            this.btnCancel.TabIndex = 14;
+            this.btnCancel.Text = "Cancel";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(12, 439);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(432, 251);
+            this.listBox1.TabIndex = 15;
+            // 
+            // cbxCompressionLevel
+            // 
+            this.cbxCompressionLevel.FormattingEnabled = true;
+            this.cbxCompressionLevel.Items.AddRange(new object[] {
+            "0",
+            "1",
+            "3",
+            "5",
+            "7",
+            "9"});
+            this.cbxCompressionLevel.Location = new System.Drawing.Point(308, 142);
+            this.cbxCompressionLevel.Name = "cbxCompressionLevel";
+            this.cbxCompressionLevel.Size = new System.Drawing.Size(45, 21);
+            this.cbxCompressionLevel.TabIndex = 16;
+            this.cbxCompressionLevel.SelectedIndexChanged += new System.EventHandler(this.cbxCompressionLevel_SelectedIndexChanged);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(235, 145);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(67, 13);
+            this.label4.TabIndex = 17;
+            this.label4.Text = "Compression";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(456, 697);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.cbxCompressionLevel);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.lblFileStatus);
+            this.Controls.Add(this.btnBuild);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btnOutput);
+            this.Controls.Add(this.txbOutput);
+            this.Controls.Add(this.cbxCompressGame);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnGamePath);
+            this.Controls.Add(this.txbGamePath);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnCERootBrowse);
+            this.Controls.Add(this.txbCERoot);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Hearts Of Oak Packager";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.FolderBrowserDialog fbdCERoot;
+        private System.Windows.Forms.TextBox txbCERoot;
+        private System.Windows.Forms.Button btnCERootBrowse;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem tsbSettings;
+        private System.Windows.Forms.ToolStripMenuItem saveToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem loadToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.Button btnGamePath;
+        private System.Windows.Forms.TextBox txbGamePath;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.CheckBox cbxCompressGame;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnOutput;
+        private System.Windows.Forms.TextBox txbOutput;
+        private System.ComponentModel.BackgroundWorker bwpMove;
+        private System.Windows.Forms.Button btnBuild;
+        private System.Windows.Forms.Label lblFileStatus;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Timer tmrMtc;
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ComboBox cbxCompressionLevel;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,297 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using System.Diagnostics;
+
+namespace Hearts_of_Oak_Packager
+{
+    public partial class Form1 : Form
+    {
+        int cLevel;
+        
+        public Form1()
+        {
+            InitializeComponent();
+
+            cbxCompressionLevel.SelectedItem = "9";
+        }
+
+        private void btnCERootBrowse_Click(object sender, EventArgs e)
+        {
+            fbdCERoot.ShowNewFolderButton = false;
+            fbdCERoot.ShowDialog();
+            txbCERoot.Text = fbdCERoot.SelectedPath.ToString();
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void SaveSettings()
+        {
+
+
+        }
+
+        private void btnGamePath_Click(object sender, EventArgs e)
+        {
+            fbdCERoot.ShowNewFolderButton = false;
+            fbdCERoot.ShowDialog();
+            txbGamePath.Text = fbdCERoot.SelectedPath.ToString();
+        }
+
+        private void btnOutput_Click(object sender, EventArgs e)
+        {
+            fbdCERoot.ShowNewFolderButton = true;
+            fbdCERoot.ShowDialog();
+            txbOutput.Text = fbdCERoot.SelectedPath.ToString();
+        }
+
+        private void btnBuild_Click(object sender, EventArgs e)
+        {
+            if (!bwpMove.IsBusy)
+            {
+                bwpMove.RunWorkerAsync();
+            }
+        }
+
+        
+        private void bwpMove_DoWork(object sender, DoWorkEventArgs e)
+        {
+            //DirectoryCopy(txbCERoot.Text, txbOutput.Text, false);
+            foreach (string f in Directory.GetFiles(txbCERoot.Text))
+            {
+                FileInfo fName = new FileInfo(@f);
+                if (!FileHelper.IgnoreExtensions.Any(fName.Extension.Contains) && !FileHelper.IgnoreFiles.Any(fName.Name.Contains))
+                File.Copy(f, txbOutput.Text + "\\" + fName.Name, true);
+            }
+
+            //move all of the main paths except for the game folder
+            foreach (String d in Directory.GetDirectories(txbCERoot.Text))
+            {
+                string sPath = new DirectoryInfo(@d).Name;
+                string oPath = txbOutput.Text + "\\" + sPath;
+                if (FileHelper.IncludeFolders.Any(sPath.Contains))
+                {
+                    //recursively move files from this path to the output folder
+                    DirectoryCopy(d, oPath, true);
+                }
+            };
+
+            string ogPath = txbOutput.Text + "\\Game"; 
+            if (!Directory.Exists(ogPath)) {Directory.CreateDirectory(ogPath);};
+            foreach (string d in Directory.GetDirectories(txbGamePath.Text))
+            {
+                string dPath = new DirectoryInfo(@d).Name;
+                               
+                if (bwpMove.CancellationPending) { return; };
+
+                if (cbxCompressGame.Checked)
+                {
+                    //if (!Directory.Exists(ogPath + "\\" + dPath)) { Directory.CreateDirectory(ogPath + "\\" + dPath); };
+                    //DirectoryCopy(d, ogPath + "\\" + dPath + "\\" + dPath, true);
+
+                    //bwpReport(dPath + " copied", true);
+
+                    if (!FileHelper.IgnoreCompress.Any(dPath.Contains))
+                    {
+                        if (!Directory.Exists(ogPath + "\\" + dPath)) { Directory.CreateDirectory(ogPath + "\\" + dPath); };
+                        DirectoryCopy(d, ogPath + "\\" + dPath + "\\" + dPath, true);
+
+                        bwpReport("Compressing game pak " + dPath);
+                        //if (!Directory.Exists(ogPath + "\\" + dPath + "\\" + dPath)) { Directory.CreateDirectory(ogPath + "\\" + dPath + "\\" + dPath); };
+                        //Directory.Move(ogPath + "\\" + dPath, ogPath + "\\" + dPath + "\\" + dPath);
+
+                        string excludes = "";
+                        foreach (string sI in FileHelper.IgnoreExtensions)
+                        {
+                            excludes += " -x!*" + sI;
+                        }
+
+                        foreach (string sI in FileHelper.IgnoreFiles)
+                        {
+                            excludes += " -x!" + sI;
+                        }
+
+                        string pClean = ogPath + "\\" + dPath + "\\*.* ";
+                        Process p = new Process();
+                        p.StartInfo.Verb = "runas";
+                        p.StartInfo.CreateNoWindow = true;
+                        p.StartInfo.UseShellExecute = false;
+                        p.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
+                        p.StartInfo.FileName = "7za.exe";
+                        p.StartInfo.Arguments = "a -tzip -r -mx" + cLevel + " " + ogPath + "\\" + dPath + ".pak " + pClean + excludes;
+                        p.Start();
+                        p.WaitForExit();
+                        bwpReport(dPath + " packed", true);
+                    
+                        try
+                        {
+                            Directory.Delete(ogPath + "\\" + dPath, true);
+                        }
+                        catch (Exception)
+                        {
+                            throw;
+                        }
+
+                    }
+                    else
+                    {
+                        DirectoryCopy(d, ogPath + "\\" + dPath, true);
+                        bwpReport(dPath + " copied", true);
+                    }
+                     
+
+                } 
+                else
+                {
+                    DirectoryCopy(d, ogPath + "\\" + dPath, true);
+                    bwpReport(dPath + " copied", true);
+                }
+                
+            }
+
+            //DirectoryCopy(txbGamePath.Text, txbOutput.Text + "\\Game", true);
+
+            //if (bwpMove.CancellationPending) { return; };
+
+            ////pack all of the folders in the game directory
+            //if (cbxCompressGame.Checked)
+            //{
+            //    // get the output game folder
+            //    foreach (string d in Directory.GetDirectories(txbOutput.Text + "\\Game"))
+            //    {
+            //        string zPath = new DirectoryInfo(@d).Name;
+            //        bwpReport("Compressing game pak " + zPath);
+            //        Zipper z = new Zipper();
+            //        z.ZipFile = @txbOutput.Text + "\\Game\\" + zPath + ".pak";
+            //        z.ItemList.Add(@"*.*");
+            //        z.ExcludeFollowing.Add(FileHelper.IgnoreExtensions);
+            //        z.PathInZip = enPathInZip.Relative;
+            //        z.Recurse = true;
+            //        z.Zip();
+            //    }
+            //}
+        }
+
+
+
+        private void bwpReport(string msg)
+        {
+            bwpMove.ReportProgress(0, msg);
+        }
+
+        private void bwpReport(string msg, bool logme)
+        {
+            int iP = 0;
+            if (logme) { iP = 1; };
+            bwpMove.ReportProgress(iP, msg);
+        }
+
+        private void DirectoryCopy(string sourceDirName, string destDirName, bool copySubDirs)
+        {
+            if (bwpMove.CancellationPending)
+            {
+                return;
+            }
+            // Get the subdirectories for the specified directory.
+            DirectoryInfo dir = new DirectoryInfo(sourceDirName);
+            DirectoryInfo[] dirs = dir.GetDirectories();
+
+            if (!dir.Exists)
+            {
+                bwpReport("Source directory does not exist or could not be found: "
+                    + sourceDirName);
+                //throw new DirectoryNotFoundException(
+                //    "Source directory does not exist or could not be found: "
+                //    + sourceDirName);
+            }
+
+            // If the destination directory doesn't exist, create it. 
+            if (!Directory.Exists(destDirName))
+            {
+                Directory.CreateDirectory(destDirName);
+            }
+
+            // Get the files in the directory and copy them to the new location.
+            FileInfo[] files = dir.GetFiles();
+            foreach (FileInfo file in files)
+            {
+                if (!FileHelper.IgnoreExtensions.Any(file.Extension.Contains) && !FileHelper.IgnoreFiles.Any(file.Name.Contains))
+                {
+                    string temppath = Path.Combine(destDirName, file.Name);
+                    file.CopyTo(temppath, true);
+                    bwpReport("Copying " + file.Name);
+                }
+                
+            }
+
+            // If copying subdirectories, copy them and their contents to new location. 
+            if (copySubDirs)
+            {
+                foreach (DirectoryInfo subdir in dirs)
+                {
+                    if (!FileHelper.IgnoreFolders.Any(subdir.Name.ToLower().Equals)) {
+                        string temppath = Path.Combine(destDirName, subdir.Name);
+                        DirectoryCopy(subdir.FullName, temppath, copySubDirs);
+                    }
+                }
+            }
+        }
+
+        private void bwpMove_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            if (e.ProgressPercentage == 1)
+            {
+                
+                listBox1.Items.Insert(0,e.UserState.ToString());
+            }
+            else
+            {
+                lblFileStatus.Text = e.UserState.ToString();
+            }
+
+        }
+
+        private void tmrMtc_Tick(object sender, EventArgs e)
+        {
+            if (bwpMove.IsBusy)
+            {
+                btnBuild.Enabled = false;
+                lblFileStatus.Visible = true;
+                progressBar1.Visible = true;
+                btnCancel.Enabled = true;
+                if (progressBar1.Value > 99) { progressBar1.Value = 1; }
+                progressBar1.Value += 1;
+            }
+            else
+            {
+                btnBuild.Enabled = true;
+                btnCancel.Enabled = false;
+                lblFileStatus.Visible = false;
+                progressBar1.Visible = false;
+            }
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            if (bwpMove.IsBusy)
+            {
+                bwpMove.CancelAsync();
+            }
+        }
+
+        private void cbxCompressionLevel_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            cLevel = Convert.ToInt16(cbxCompressionLevel.SelectedItem.ToString());
+        }
+
+    }
+}
